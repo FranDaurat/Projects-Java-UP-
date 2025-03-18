73a4d39d791073adc8c3dc38ee1aaf80c60b3a43
frande <frande@MacBook-Air-de-frande.local>
2025-03-18 10:20:41 -0300

    probando

---
+++ Clase2/src/Main.java
@@ -0,0 +1,10 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Song mySong = new Song("gustavo",3);
+
+        Song song;
+        song= new Song("carlos",4);
+        System.out.println(song);
+    }
+}
